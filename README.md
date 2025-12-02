9abcffd8370caacf24277d891009f94ad9b62242
Emuesiri Agbagidi <emuesirialexagbagidi@gmail.com>
2025-12-02 11:21:39 +0100

    mapping the player and computer choices

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 this is a cli-based Rock-Paper-Scissors game made with Python.
-I want to use it as an opputunity to learn git and Python
+I want to use it as an opportunity to learn git and Python
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import random
+from secrets import choice
+
+#mapping numbers to choices
+choices = {1: "Rock", 2: "Paper", 3: "Scissors"}
 
 player_choice = int(input("Select your option (1 = Rock, 2 = Paper, 3 = Scissors): "))
+player_choice = choices.get(player_choice, "Invalid choice")
 
-if player_choice == 1:
-    player_choice_name = "Rock"
-elif player_choice == 2:
-    player_choice_name = "Paper"
-else:
-    player_choice_name = "Scissors"
+
+if player_choice == "Invalid choice":
+    print("Select from 1, 2 or 3.")
+    exit()
 
 computer_choice = random.randint(1, 3)
-
-if computer_choice == 1:
-    computer_choice_name = "Rock"
-elif computer_choice == 2:
-    computer_choice_name = "Paper"
-else:
-    computer_choice_name = "Scissors"
+player_choice = choices.get(player_choice)
 
 if player_choice == 1 and computer_choice == 2:
     print("Computer Wins")
