ad6e4a30de0aacaecfaeb96b8cd64f9ec5024123
Emuesiri Agbagidi <emuesirialexagbagidi@gmail.com>
2025-12-02 12:04:20 +0100

    walahi I added  lot of features

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import random
-from secrets import choice
 
-#mapping numbers to choices
+# mapping numbers to choices
 choices = {1: "Rock", 2: "Paper", 3: "Scissors"}
 
 player_choice = int(input("Select your option (1 = Rock, 2 = Paper, 3 = Scissors): "))
-player_choice = choices.get(player_choice, "Invalid choice")
-
+player_choice_name = choices.get(player_choice, "Invalid choice")
 
 if player_choice == "Invalid choice":
     print("Select from 1, 2 or 3.")
     exit()
 
 computer_choice = random.randint(1, 3)
-player_choice = choices.get(player_choice)
+computer_choice_name = choices.get(computer_choice)
 
-if player_choice == 1 and computer_choice == 2:
-    print("Computer Wins")
-elif player_choice == 2 and computer_choice == 3:
-    print("Computer Wins")
-elif player_choice == 3 and computer_choice == 1:
-    print("Computer Wins")
-elif player_choice == computer_choice:
-    print("Draw")
-else:
+# display the choices
+print(f"The computer choose: {computer_choice_name}.")
+print(f"You choose: {player_choice_name}.")
+
+if player_choice == computer_choice:
+    print("It's a draw!")
+
+elif (player_choice == 1 and computer_choice == 3) or (player_choice == 2 and computer_choice == 1) or (
+        player_choice == 3 and computer_choice == 2):
     print("You win!")
-
-# print(f"Player choice: {player_choice}")
-# print(f"Player choice: {player_choice_name}")
-# print(f"Computer choice: {computer_choice}")
-# print(f"Computer choice: {computer_choice_name}")
+else:
+    print("Computer wins!")
