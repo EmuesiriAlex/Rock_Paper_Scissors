ed12c257408e813dd3d369fd532242c25e15ba91
Emuesiri Agbagidi <emuesirialexagbagidi@gmail.com>
2025-12-02 10:56:14 +0100

    game test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+import random
+
+player_choice = input("Select your option (1 = Rock, 2 = Paper, 3 = Scissors): ")
+
+computer_choice = random.randrange(2)
+
+if player_choice == 1 and computer_choice == 2:
+    print("Computer Wins")
+elif player_choice == 2 and computer_choice == 3:
+    print("Computer Wins")
+elif player_choice == 3 and computer_choice == 1:
+    print("Computer Wins")
+elif player_choice == computer_choice:
+    print("Draw")
+else:
+    print("You win!")
